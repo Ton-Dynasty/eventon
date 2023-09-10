abb1b855ee53fc6e8c5e12af2ba9b42963e7e6e2
ipromise2324 <168allen55@gmail.com>
2023-09-10 22:08:44 +0800

    feat: add detecting vulnerable contract test

--- tests/BugDetector.spec.ts
+++ tests/BugDetector.spec.ts
@@ -1,19 +1,55 @@
-import { Blockchain, SandboxContract, TreasuryContract } from '@ton-community/sandbox';
-import { beginCell, toNano } from 'ton-core';
+import { Blockchain, SandboxContract, TreasuryContract, printTransactionFees } from '@ton-community/sandbox';
+import { Address, Cell, address, beginCell, toNano } from 'ton-core';
 import { BugDetector } from '../wrappers/BugDetector';
 import '@ton-community/test-utils';
-import { EventSignal, ProtcolRegister, SubscribeBody, UniversalRouter } from '../wrappers/UniversalRouter';
+import { BugWarning, EventSignal, ProtcolRegister, SubscribeBody, UniversalRouter } from '../wrappers/UniversalRouter';
 import { ChildRouter, CreateBody, DeleteSubscriber } from '../wrappers/ChildRouter';
 import { Event, EventTrigger } from '../wrappers/Event';
 import { Messenger } from '../wrappers/Messenger';
 import { Follower } from '../wrappers/Follower';
 import exp from 'constants';
+import { UserDefaultCallback } from '../wrappers/UserDefaultCallback';
 describe('BugDetector', () => {
     let blockchain: Blockchain;
     let bugDetector: SandboxContract<BugDetector>;
     let universalRouter: SandboxContract<UniversalRouter>;
     let owner: SandboxContract<TreasuryContract>;
+    let userDefaultCallback: SandboxContract<UserDefaultCallback>;
+    async function protocolRegsiter() {
+        const protocolRegister: ProtcolRegister = {
+            $$type: 'ProtcolRegister',
+            maxUserStakeAmount: toNano('100'),
+            subscribeFeePerTick: toNano('0.5'),
+            sourceAddress: bugDetector.address, // oracle event
+            template: beginCell().endCell(),
+        };
+        const registerResult = await bugDetector.send(
+            owner.getSender(),
+            {
+                value: toNano('0.5'),
+            },
+            protocolRegister
+        );
+    }
 
+    async function userRegsiter(eventId: bigint, user: SandboxContract<TreasuryContract>, callbackAddress: Address) {
+        //await protocolRegsiter(oracle.address, trader); // Simply call the function to handle the registration
+        const subscribeBody: SubscribeBody = {
+            $$type: 'SubscribeBody',
+            walletAddress: user.address, // Owner address of callback contract
+            deadline: 100n, // The deadline of the msg can delay
+            eventId: eventId, // The even id which user want to subscribe
+            callbackAddress: callbackAddress, // Callback contract address written by user
+        };
+
+        await universalRouter.send(
+            user.getSender(),
+            {
+                value: toNano('10'),
+            },
+            subscribeBody
+        );
+    }
     beforeEach(async () => {
         blockchain = await Blockchain.create();
         owner = await blockchain.treasury('owner');
@@ -66,13 +102,15 @@ describe('BugDetector', () => {
             sourceAddress: bugDetector.address, // oracle event
             template: beginCell().endCell(),
         };
+        const eventIdBefore = await universalRouter.getEventId();
         const registerResult = await bugDetector.send(
             owner.getSender(),
             {
                 value: toNano('0.5'),
             },
             protocolRegister
-        ); 
+        );
+        const eventIdAfter = await universalRouter.getEventId();
         
         // Test whether the register msg is sent by owner
         expect(registerResult.transactions).toHaveTransaction({
@@ -88,6 +126,72 @@ describe('BugDetector', () => {
             success: true,
         });
 
+        // Test whether the event id is increased by 1
+        expect(eventIdAfter).toBe(eventIdBefore + 1n);
+        printTransactionFees(registerResult.transactions);
+    });
 
+    it('should bug detectoer should send event signal to universal router', async () => {
+        await protocolRegsiter();
+        const childRouterAddress = await universalRouter.getChildRouterAddress(bugDetector.address);
+        const childRouter = blockchain.openContract(ChildRouter.fromAddress(childRouterAddress));
+        const messagerAddress = await childRouter.getMessengerAddress(bugDetector.address, 0n);
+        const messenger = blockchain.openContract(await Messenger.fromAddress(messagerAddress));
+        let user = await blockchain.treasury('user');
+        userDefaultCallback = blockchain.openContract(await UserDefaultCallback.fromInit(childRouterAddress, user.address, beginCell().endCell()));
+        const deployResultUdc = await userDefaultCallback.send(
+            owner.getSender(),
+            {
+                value: toNano('1'),
+            },
+            {
+                $$type: 'Deploy',
+                queryId: 0n,
+            }
+        );
+        await userRegsiter(0n, user, userDefaultCallback.address);
+
+        // Bug Detector send event signal to the user default callback contract
+        let badContract = await blockchain.treasury('badContract');
+        let bugInfo: Cell = beginCell().storeBuffer(Buffer.from('Reentrancy')).endCell();
+        const bugWarning: BugWarning = {
+            $$type: 'BugWarning',
+            targetAdress: badContract.address,
+            bugInfo: bugInfo,
+        };
+
+        const bugWarningResult = await bugDetector.send(
+            owner.getSender(),
+            {
+                value: toNano('10'),
+            },
+            bugWarning
+        );
+        //printTransactionFees(bugWarningResult.transactions);
+        expect(bugWarningResult.transactions).toHaveTransaction({
+            from: bugDetector.address,
+            to: universalRouter.address,
+            success: true,
+        });
+
+        expect(bugWarningResult.transactions).toHaveTransaction({
+            from: universalRouter.address,
+            to: childRouterAddress,
+            success: true,
+        });
+
+        expect(bugWarningResult.transactions).toHaveTransaction({
+            from: childRouterAddress,
+            to: messagerAddress,
+            success: true,
+        });
+
+        expect(bugWarningResult.transactions).toHaveTransaction({
+            from: messagerAddress,
+            to: userDefaultCallback.address,
+            success: true,
+        });
     });
 });
+
+
