b00c704a48c91f9f5260f34288134efc75768c1b
alan890104 <alan890104@gmail.com>
2023-09-12 22:50:28 +0800

    - feat(gen): add .gitignore file - feat(gen): add generateMnemonic.ts file with functionality to generate mnemonic and save it to .env.test file

---
+++ gen/generateMnemonic.ts
@@ -0,0 +1,24 @@
+// import required libraries
+import * as fs from 'fs';
+import { WalletContractV4 } from 'ton';
+import { mnemonicNew, mnemonicToWalletKey } from 'ton-crypto';
+
+const run = async () => {
+    try {
+        const mnemonic = await mnemonicNew(24);
+        console.log('Generated Mnemonic:', mnemonic);
+
+        // Save to .env file
+        const envData = `WALLET_MNEMONIC=${mnemonic}\nWALLET_VERSION=v4\n`;
+        fs.writeFileSync('.env.test', envData);
+        console.log('Mnemonic saved to .env.test file.');
+        const keypair = await mnemonicToWalletKey(mnemonic);
+        console.log('Public Key:', keypair.publicKey);
+        console.log('Secret Key:', keypair.secretKey);
+        console.log('Address:', WalletContractV4.create({ workchain: 0, publicKey: keypair.publicKey }).address);
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+};
+
+run();
